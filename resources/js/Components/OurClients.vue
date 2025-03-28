f13abd622e1c3519a37553bf0fb6d42fb12ce607
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-03-28 13:43:44 +0700

    task: create component our clients section

--- resources/js/Components/OurClients.vue
+++ resources/js/Components/OurClients.vue
@@ -1,30 +1,31 @@
 <template>
     <div class="container mx-auto px-4">
-        <!-- Section Heading -->
         <div class="text-center mb-12">
-            <h1 class="text-2xl font-bold">{{ title }}</h1>
-            <p>{{ subtitle }}</p>
+            <Heading1 class="text-gray-900">
+                {{ title }}
+            </Heading1>
+            <Paragraph class="text-gray-600 mt-2">
+                {{ subtitle }}
+            </Paragraph>
         </div>
-
-        <!-- Swiper Carousel -->
         <swiper
             :slides-per-view="2"
             :space-between="30"
             :loop="true"
-            :autoplay="{ delay: 10, disableOnInteraction: false }"
+            :autoplay="{ delay: 3000, disableOnInteraction: false }"
             :breakpoints="{
-        640: { slidesPerView: 5 },
-        768: { slidesPerView: 5 },
-        1024: { slidesPerView: 5 }
-      }"
+                640: { slidesPerView: 5 },
+                768: { slidesPerView: 5 },
+                1024: { slidesPerView: 5 }
+            }"
             class="mySwiper"
         >
             <swiper-slide v-for="(client, index) in clients" :key="index">
                 <div class="w-24 md:w-28 lg:w-32 flex items-center justify-center">
-                    <img
+                    <LazyImage
                         :src="client.logo"
-                        :alt="client.name"
-                        class="h-12 object-contain transition-all duration-300"
+                        :alt="client.name || 'Client logo'"
+                        class="h-12 object-contain transition-all duration-300 hover:scale-110"
                     />
                 </div>
             </swiper-slide>
@@ -38,10 +39,15 @@ import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
 import "swiper/css/autoplay";
 
+import Heading1 from "@/Components/Typography/Heading1.vue";
+import Paragraph from "@/Components/Typography/Paragraph.vue";
+
 export default defineComponent({
     components: {
         Swiper,
         SwiperSlide,
+        Heading1,
+        Paragraph
     },
     props: {
         title: {
