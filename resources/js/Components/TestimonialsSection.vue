4504c3d67bd095bdc073a26452fbad5c171b9e7e
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-03-28 23:04:19 +0700

    fix: refactored testimonials section: improved alt text, renamed props, used computed class, and replaced h4

--- resources/js/Components/TestimonialsSection.vue
+++ resources/js/Components/TestimonialsSection.vue
@@ -1,19 +1,19 @@
 <template>
     <div v-for="(testimonial, index) in testimonials" :key="index" class="flex items-start space-x-4">
-        <div v-if="showAvatars" class="flex-shrink-0">
+        <div v-if="showAvatar" class="flex-shrink-0">
             <LazyImage
                 :src="testimonial.avatar"
                 :alt="`${testimonial.name} avatar`"
-                :class="['object-cover border-2 border-gray-200', avatarSize, avatarShape]"
+                :class="avatarClasses"
             />
         </div>
         <div>
             <Paragraph class="italic">
-                <span v-if="showQuotationMarks">“</span>
+                <span v-if="showQuot">“</span>
                 {{ testimonial.quote }}
-                <span v-if="showQuotationMarks">”</span>
+                <span v-if="showQuot">”</span>
             </Paragraph>
-            <h4 class="text-lg font-semibold mt-2">{{ testimonial.name }}</h4>
+            <Heading4>{{ testimonial.name }}</Heading4>
             <Paragraph class="text-sm">
                 {{ testimonial.title }}
             </Paragraph>
@@ -22,8 +22,9 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { defineProps, computed } from "vue";
 import Paragraph from "@/Components/Typography/Paragraph.vue";
+import Heading4 from '@/Components/Typography/Heading4.vue';
 
 interface Testimonial {
     avatar: string;
@@ -32,13 +33,19 @@ interface Testimonial {
     title: string;
 }
 
-defineProps<{
+const props = defineProps<{
     title?: string;
     testimonials: Testimonial[];
-    showAvatars?: boolean;
-    showQuotationMarks?: boolean;
+    showAvatar?: boolean;
+    showQuot?: boolean;
     avatarSize?: string;
     avatarShape?: string;
     gridCols?: string;
 }>();
+
+const avatarClasses = computed(() => [
+    "object-cover border-2 border-gray-200",
+    props.avatarSize,
+    props.avatarShape
+]);
 </script>
