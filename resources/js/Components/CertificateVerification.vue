e2184d5fb5fe63215eb54e229403925ee40eb074
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-04-04 11:14:29 +0700

    fix : change to use toast of primevue

--- resources/js/Components/CertificateVerification.vue
+++ resources/js/Components/CertificateVerification.vue
@@ -1,4 +1,5 @@
 <template>
+    <Toast position="bottom-right" />
     <div class="w-full flex justify-center lg:px-24 md:px-8">
         <div class="w-full max-w-8xl h-full bg-white shadow-lg rounded-lg grid lg:grid-cols-5 grid-cols-1">
             <div class="flex items-center justify-center lg:col-span-2 col-span-1 p-6 lg:p-8">
@@ -15,21 +16,7 @@
                 <div class="space-y-6 lg:space-y-8">
                     <div>
                         <label for="country" class="text-gray-600 text-lg lg:text-xl block mb-2">Country Name</label>
-                        <div class="relative">
-                            <select
-                                id="country"
-                                v-model="country"
-                                class="w-full py-3 px-4 lg:py-4 lg:px-5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 appearance-none text-lg"
-                            >
-                                <option value="" disabled>Select Country</option>
-                                <option v-for="c in countries" :key="c" :value="c">{{ c }}</option>
-                            </select>
-                            <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                                <svg class="w-5 h-5 lg:w-6 lg:h-6 text-gray-400" fill="none"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                                </svg>
-                            </div>
-                        </div>
+                        <SelectLabel v-model="country" css-wrapper="w-full  py-1.5 px-4 lg:py-2 lg:px-2 " />
                     </div>
                     <div>
                         <label for="certificate" class="text-gray-600 text-lg lg:text-xl block mb-2">Enter Certificate Number</label>
@@ -55,13 +42,15 @@
 
 <script>
 import Heading2 from '@/Components/Typography/Heading2.vue';
-
+import { useToast } from 'primevue/usetoast';
+import Toast from 'primevue/toast';
+import SelectLabel from '@/Components/SelectLabel.vue';
 export default {
     name: 'CertificateVerification',
-    components: { Heading2 },
+    components: { SelectLabel, Heading2, Toast },
     data() {
         return {
-            country: '',
+            country: "",
             certificateNumber: '',
             countries: [
                 'United States',
@@ -75,24 +64,28 @@ export default {
                 'India',
                 'Brazil'
             ]
-        }
+        };
+    },
+    setup() {
+        const toast = useToast();
+        return { toast };
     },
     methods: {
         verifyNow() {
             if (!this.country) {
-                alert('Please select a country');
+                this.toast.add({ severity: 'warn', summary: 'Warning', detail: 'Please select a country', life: 3000 });
                 return;
             }
             if (!this.certificateNumber) {
-                alert('Please enter a certificate number');
+                this.toast.add({ severity: 'warn', summary: 'Warning', detail: 'Please enter a certificate number', life: 3000 });
                 return;
             }
-            console.log(`Verifying certificate ${this.certificateNumber} from ${this.country}`);
             this.$emit('verify', {
                 country: this.country,
                 certificateNumber: this.certificateNumber
             });
         }
     }
-}
+};
 </script>
+
