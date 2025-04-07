e086aded0a2d332425c2f7aeaa0eaced2378b11c
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-04-07 16:41:47 +0700

    fix: text overflow in service card

--- resources/js/Components/Services.vue
+++ resources/js/Components/Services.vue
@@ -7,25 +7,33 @@
         <LazyImage
             :src="service.imageUrl"
             :alt="service.title"
-            class="w-full h-48 object-cover transition-transform duration-300 hover:scale-110"
+            class="w-full h-48 object-cover transition-transform duration-300 hover:scale-100"
         />
-        <div class="p-4">
-            <Heading2 class="text-center border-b pb-2 mb-3">
-                {{ service.title }}
-            </Heading2>
-            <Paragraph class="text-sm mb-4">
-                {{ service.description }}
-            </Paragraph>
-            <Button @click="handleClick(service)" class="w-full bg-blue-600 hover:bg-blue-700 py-3" >Read More</Button>
+        <div class="p-4 flex flex-col h-[210px] justify-between">
+            <div>
+                <Heading2 class="text-center border-b pb-2 mb-3">
+                    {{ service.title }}
+                </Heading2>
+                <Paragraph class="text-sm mb-4 line-clamp-3 text-justify">
+                    {{ truncate(service.description, 150) }}
+                </Paragraph>
+            </div>
+            <Button
+                icon=""
+                type="button"
+                text="Read More"
+                @click="() => handleClick"
+                class="w-full"
+            />
         </div>
     </div>
 </template>
-
 <script setup lang="ts">
-import { defineProps, defineEmits  } from "vue";
+import { defineProps, defineEmits } from "vue";
 import Paragraph from "@/Components/Typography/Paragraph.vue";
 import Heading2 from "@/Components/Typography/Heading2.vue";
-import Button from '@/Components/Button.vue';
+import Button from "@/Components/Common/Button.vue";
+
 interface Service {
     id: number;
     imageUrl: string;
@@ -37,11 +45,16 @@ defineProps<{
     title?: string;
     services: Service[];
 }>();
+
 const emit = defineEmits<{
     (event: 'read-more', service: Service): void;
 }>();
 
 const handleClick = (service: Service) => {
     emit('read-more', service);
+};
+
+const truncate = (text: string, max: number) => {
+    return text.length > max ? text.slice(0, max) + "..." : text;
 };
 </script>
