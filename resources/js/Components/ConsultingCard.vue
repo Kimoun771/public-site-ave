7522e31a5929db35e86573528aa65795d9df3d9d
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-04-08 10:17:47 +0700

    task: create card for training and consulting card
    fix: change default: true to default: "" and key="index"

---
+++ resources/js/Components/ConsultingCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full p-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <ProgramCard
+                v-for="(service, index) in consultingServices"
+                :key="index"
+                :image="service.image"
+                :title="service.title"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import ProgramCard from "./ProgramCard.vue";
+const props = defineProps({
+    consultingServices: {
+        type: Array,
+        default: ""
+    },
+});
+</script>
---
+++ resources/js/Components/TrainingCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-full p-6">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <ProgramCard
+                    v-for="(program, index) in trainingPrograms"
+                    :key="index"
+                    :image="program.image"
+                    :title="program.title"
+                />
+            </div>
+    </div>
+</template>
+
+<script setup>
+import ProgramCard from "./ProgramCard.vue";
+import Heading2 from '@/Components/Typography/Heading2.vue';
+
+const props = defineProps({
+    trainingPrograms: {
+        type: Array,
+        default: true
+    },
+});
+</script>
