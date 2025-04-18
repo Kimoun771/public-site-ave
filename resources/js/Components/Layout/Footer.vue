68f1e4a4d6191fe593415c0724680e7c322adf3a
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-04-18 13:17:36 +0700

    task: create header and footer

--- resources/js/Components/Layout/Footer.vue
+++ resources/js/Components/Layout/Footer.vue
@@ -1,39 +1,67 @@
 <template>
-    <footer class="py-14 text-slate-500  dark:text-slate-400">
-      <Container class="flex flex-col gap-10">
-        <div class="flex flex-col items-center gap-6">
-          <Link :href="route('welcome')">
-            <Logo class="h-10" />
-          </Link>
-          <nav class="flex flex-col sm:flex-row items-center justify-center gap-3">
-            <Link
-              v-for="menu in menuItems"
-              :key="menu.label"
-              :href="menu.link"
-              class="rounded-lg py-1.5 px-3 transition-colors duration-300 hover:bg-slate-100 dark:hover:bg-slate-800"
-            >
-            {{ $t(menu.label) }}
-            </Link>
-          </nav>
+    <footer class="bg-gray-800 text-white py-4 sm:py-8 lg:py-8" menu-items="">
+        <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
+            <div class="col-span-1 flex flex-col items-center px-8">
+                <Link :href="route('home')">
+                    <LazyImage src="https://www.vecert.com/image/vecert-logo.png" alt="VE CERT Logo" class="h-16 mb-4"/>
+                </Link>
+                <Paragraph class="text-sm mb-4 text-center text-white">Copyright © 2025 AVE. All rights reserved.</Paragraph>
+                <div class="flex space-x-4">
+                    <a href="https://instagram.com/vecert" target="_blank" class="text-white hover:text-gray-100">
+                        <i class="pi pi-facebook text-xl"></i>
+                    </a>
+                    <a href="https://instagram.com/vecert" target="_blank" class="text-white hover:text-gray-100">
+                        <i class="pi pi-instagram text-xl"></i>
+                    </a>
+                    <a href="https://twitter.com/vecert" target="_blank" class="text-white hover:text-gray-300">
+                        <i class="pi pi-twitter text-xl"></i>
+                    </a>
+                    <a href="https://youtube.com/vecert" target="_blank" class="text-white hover:text-gray-300">
+                        <i class="pi pi-youtube text-xl"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-span-1">
+                <nav class="flex flex-col gap-2">
+                    <Link
+                        v-for="menu in menuItems"
+                        :key="menu.label"
+                        :href="menu.link"
+                        class="text-white text-center hover:text-gray-300 transition-colors duration-300"
+                    >
+                        {{ menu.label }}
+                    </Link>
+                </nav>
+            </div>
+
+            <div class="col-span-1 sm:col-span-3 lg:col-span-3">
+                <form class="space-y-2 px-5">
+                    <div class="flex flex-col sm:flex-row gap-2">
+                        <input type="text" placeholder="Name" class="flex-1 px-3 py-2 bg-gray-100 text-black rounded" />
+                        <input type="email" placeholder="Email" class="flex-1 px-3 py-2 bg-gray-100 text-black rounded" />
+                    </div>
+                    <textarea placeholder="Message" class="w-full px-3 py-2 bg-gray-100 text-black rounded h-24"></textarea>
+                    <button class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Submit</button>
+                </form>
+            </div>
         </div>
-        <hr class="border-slate-200 dark:border-slate-700" />
-        <div class="flex items-center text-center justify-between flex-col sm:flex-row gap-4">
-          <p v-text="'Copyright © 2025 AVE. All rights reserved.'" />
-        </div>
-      </Container>
     </footer>
-  </template>
+</template>
 
-  <script setup>
-  import Container from "@/Components/Container.vue";
-  import Logo from "@/Components/Icons/Logo.vue";
-  import IconGithub from "@/Components/Icons/IconGithub.vue";
-  import { Link } from '@inertiajs/vue3';
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import Paragraph from '@/Components/Typography/Paragraph.vue';
 
+interface MenuItem {
+    label: string;
+    link: string;
+}
 
-  const props = defineProps({
-    menuItems: Array,
-    laravelVersion: String,
-    phpVersion: String,
-  });
-  </script>
+const props = defineProps({
+    menuItems: {
+        type: Array as () => MenuItem[],
+        required: true
+    }
+});
+</script>
--- resources/js/Components/Layout/MobileNav.vue
+++ resources/js/Components/Layout/MobileNav.vue
@@ -3,26 +3,26 @@
     <div class="md:hidden">
         <button class="p-2 md:hidden relative z-10" @click.prevent="toggleMobileMenu">
             <component :is="showMobileMenu ? IconX : IconBars" class="w-5"
-                :class="{ 'text-red-500 dark:text-red-300': showMobileMenu }" />
+                       :class="{ 'text-red-500 dark:text-red-300': showMobileMenu }" />
         </button>
 
         <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" class="transition-opacity duration-500">
             <div v-show="showMobileMenu" @click.prevent="toggleMobileMenu"
-                class="fixed inset-0 bg-slate-200/50 dark:bg-slate-800/50 backdrop-blur-sm" />
+                 class="fixed inset-0 bg-slate-200/50 dark:bg-slate-800/50 backdrop-blur-sm" />
         </Transition>
 
         <Transition enter-from-class="-translate-x-full opacity-0" leave-to-class="-translate-x-full opacity-0"
-            class="transition-all duration-500">
+                    class="transition-all duration-500">
             <div @click.stop v-show="showMobileMenu"
-                class="p-4 absolute inset-x-4 top-14 bg-white dark:bg-slate-900 rounded-xl shadow-lg">
+                 class="p-4 absolute inset-x-4 top-14 bg-white dark:bg-slate-900 rounded-xl shadow-lg">
                 <nav class="flex flex-col">
                     <button v-for="menu in menuItems" :key="menu.label" v-text="menu.label"
-                        @click.prevent="redirect(menu.link)"
-                        class="p-4 hover:bg-slate-100 dark:hover:bg-slate-800 rounded-lg text-left" />
+                            @click.prevent="redirect(menu.link)"
+                            class="p-4 hover:bg-slate-100 dark:hover:bg-slate-800 rounded-lg text-left" />
                     <hr class="my-2 border-slate-200 dark:border-slate-700" />
                     <Link class="p-4 hover:bg-slate-100 dark:hover:bg-slate-800 rounded-lg text-left"
-                        :href="route('login')" v-if="canLogin && !$page.props.auth.user">
-                    Login
+                          :href="route('login')" v-if="canLogin && !$page.props.auth.user">
+                        Login
                     </Link>
                     <div class="flex flex-col gap-2" v-if="$page.props.auth.user">
                         <div class="py-6 px-4">
@@ -33,18 +33,6 @@
                                 {{ $page.props.auth.user.email }}
                             </div>
                         </div>
-                        <Link :href="route('dashboard')"
-                            class="p-4 hover:bg-slate-100 dark:hover:bg-slate-800 rounded-lg text-left">
-                        Dashboard
-                        </Link>
-                        <Link :href="route('profile.edit')"
-                            class="p-4 hover:bg-slate-100 dark:hover:bg-slate-800 rounded-lg text-left">
-                        Profile
-                        </Link>
-                        <Link :href="route('logout')" method="post" as="button"
-                            class="p-4 hover:bg-slate-100 dark:hover:bg-slate-800 rounded-lg text-left">
-                        Log Out
-                        </Link>
                     </div>
                 </nav>
             </div>
