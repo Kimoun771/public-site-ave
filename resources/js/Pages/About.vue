0d06ac5604311438cee53ebde897de3a2dfbe603
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-04-22 09:44:17 +0700

    task: create page contact us, about use and training

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,32 @@
+<script setup>
+import GeneralLayout from '@/Layouts/GeneralLayout.vue';
+import Vision from '@/Components/Vision.vue';
+import WhoWeAre from '@/Components/WhoWeAre.vue';
+import HeroImage from '@/Components/HeroImage.vue';
+import Heading1 from '@/Components/Typography/Heading1.vue';
+</script>
+
+<template>
+    <GeneralLayout laravel-version="" php-version="">
+        <HeroImage background-image="https://www.bsigroup.com/globalassets/localfiles/1400x585/amazon-river-1400x585-reversed.jpg"
+                   title="Ensuring Quality & Compliance Through Expert Inspections!"
+                   description="We provide independent inspection and verification services to help businesses meet industry standards and regulatory requirements."
+        ></HeroImage>
+        <Heading1 class="text-center mt-10">Welcome to About Page</Heading1>
+        <div class="lg:px-24">
+            <Vision img="https://www.vecert.com/images/665dcccc9499e_600w.png"
+                    title="Ensuring Compliance and Safety"
+                    imageAlt="Compliance and Safety Image"
+                    description="At VE Cert, we specialize in certification, inspection, and training services, ensuring businesses meet global compliance and safety standards. Our goal is to enhance credibility, efficiency, and operational excellence through trusted and transparent certification solutions."
+            >
+            </Vision>
+        </div>
+        <div class="lg:px-24">
+            <WhoWeAre img="https://www.vecert.com/images/665dcccc9499e_600w.png"
+                      title="Who We Are"
+                      imageAlt="Who We Are"
+                      description="VE Cert provides certification, inspection, and consulting services to ensure compliance with international standards."
+            ></WhoWeAre>
+        </div>
+    </GeneralLayout>
+</template>
---
+++ resources/js/Pages/Training.vue
@@ -0,0 +1,62 @@
+<script setup>
+import GeneralLayout from '@/Layouts/GeneralLayout.vue';
+import ConsultingCard from '@/Components/ConsultingCard.vue'
+import TrainingCard from '@/Components/TrainingCard.vue';
+import Heading2 from '@/Components/Typography/Heading2.vue';
+import HeroImage from '@/Components/HeroImage.vue';
+const trainingPrograms = [
+    {
+        title: "Software Development Bootcamp",
+        description: "Learn full-stack development in 12 weeks.",
+        image: "https://www.vecert.com/images/662ccddf47cb8_600w.jpg"
+    },
+    {
+        title: "Data Science Training",
+        description: "Master data analysis and machine learning.",
+        image: "https://www.vecert.com/images/665dcd3e80ead_600w.png"
+    }
+    ,
+    {
+        title: "Data Science Training",
+        description: "Master data analysis and machine learning.",
+        image: "https://www.vecert.com/images/665dcd6b2e572_600w.png"
+    }
+];
+
+const consultingServices = [
+
+    {
+        title: "Cybersecurity Assessment",
+        description: "Identify vulnerabilities and secure your systems.",
+        image: "https://www.vecert.com/images/665dcd3e80ead_600w.png"
+    }
+    ,
+    {
+        title: "Cybersecurity Assessment",
+        description: "Identify vulnerabilities and secure your systems.",
+        image: "https://www.vecert.com/images/665dcd6b2e572_600w.png"
+    }, {
+        title: "Business Strategy Consulting",
+        description: "Optimize your business strategy for growth.",
+        image: "https://www.vecert.com/images/662ccddf47cb8_600w.jpg"
+    }
+];
+</script>
+
+<template>
+    <GeneralLayout laravel-version="11" php-version="">
+        <HeroImage
+            backgroundImage="https://www.bsigroup.com/globalassets/localfiles/1400x585/amazon-river-1400x585-reversed.jpg"
+            title="Ensuring Quality & Compliance Through Expert Inspections!"
+            description="We provide independent inspection and verification services to help businesses meet industry standards and regulatory requirements."
+        />
+        <div class="mt-5">
+            <Heading2 class="text-center mb-6"> Training Programs</Heading2>
+            <TrainingCard :trainingPrograms="trainingPrograms"></TrainingCard>
+        </div>
+        <div class="mt-5" >
+            <Heading2 class="text-center mb-6"> Consulting Services</Heading2>
+            <ConsultingCard :consulting-services="consultingServices"></ConsultingCard>
+        </div>
+    </GeneralLayout>
+</template>
