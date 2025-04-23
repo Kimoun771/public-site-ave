320bde2b5445293531c4ad8b1fa1c5deb22ad01a
sovouthea33 <sovouthea90@gmail.com>
2025-04-23 10:04:30 +0700

    task: create popup

---
+++ resources/js/Components/Popup.vue
@@ -0,0 +1,50 @@
+<script setup>
+  import Dialog from 'primevue/dialog'
+  import { defineModel, defineProps } from 'vue'
+
+
+  const visible = defineModel()
+  const props = defineProps({
+    title: { type: String, default: '' },
+    des: { type: String, default: '' },
+    image: { type: String, default: '' }
+  })
+
+  const closeModal = () => {
+    visible.value = false
+  }
+
+</script>
+
+<template>
+  <Dialog
+    v-model:visible="visible"
+    :header="title"
+    :style="{ width: '50rem'}"
+    pt:mask:class="backdrop-blur-sm"
+    :dismissableMask="true"
+    @hide="closeModal"
+    escape
+    maximizable
+    modal
+  >
+    <div class="pb-5 space-y-5">
+      <LazyImage
+        v-if="image"
+        :src="image"
+        :alt="title"
+          class="w-full h-auto rounded-lg object-cover"
+      />
+      <div class="prose prose-gray max-w-none">
+        <div v-html="des"></div>
+      </div>
+    </div>
+  </Dialog>
+</template>
+
+<style>
+  ol {
+    list-style:  disc;
+    margin-left: 3%;
+  }
+</style>
