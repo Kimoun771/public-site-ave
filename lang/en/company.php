53e00ab5889100658378390f6cd62d39e92b43bb
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-04-30 16:59:10 +0700

    feature: crud company

---
+++ lang/en/company.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'company_information' => 'Company Information',
+    'company_name' => 'Company Name',
+    'scheme' => 'Scheme',
+    'scope' => 'Scope of Company',
+    'address' => 'Company Address',
+    'country' => 'Country',
+    'company' => 'Company',
+    'plural' => 'Companies',
+];
