321a465cbaaa143a00ea8a65438032c74e481cb2
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-04-30 16:45:50 +0700

    feature: add Country module with migration, seeder, resource, and translations

---
+++ app/Filament/Resources/CountryResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CountryResource\Pages;
+use App\Filament\Resources\CountryResource\RelationManagers;
+use App\Models\Company;
+use App\Models\Country;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CountryResource extends Resource
+{
+    protected static ?string $model = Country::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-globe-asia-australia';
+    public static function getNavigationGroup(): ?string
+    {
+        return __('navigation.master_data');
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('country.title');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __('country.plural');
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return __('country.plural');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label(__('country.name'))
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label(__('country.name'))
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('code')
+                    ->label(__('country.code'))
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label(__('country.created_at'))
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCountries::route('/'),
+            'create' => Pages\CreateCountry::route('/create'),
+            'edit' => Pages\EditCountry::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ lang/en/country.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Country',
+    'plural' => 'Countries',
+    'name' => 'Country Name',
+    'code' => 'Country Code',
+    'created_at' => 'Created At',
+    'main_data' => 'Main Data',
+];
