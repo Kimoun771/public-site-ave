cef95c8a6a1071b82861c7b81fa7ac37eb506631
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-05-02 16:55:38 +0700

    feature: View and list certificate suspended

---
+++ app/Filament/Resources/CertificateSuspendedResource/Pages/CreateCertificateSuspended.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CertificateSuspendedResource\Pages;
+
+use App\Filament\Resources\CertificateSuspendedResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCertificateSuspended extends CreateRecord
+{
+    protected static string $resource = CertificateSuspendedResource::class;
+}
---
+++ lang/en/certificate_suspended.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Suspended Certificate',
+    'plural' => 'Suspended Certificates',
+    'certificate_number' => 'Certificate Number',
+    'company' => 'Company',
+    'registration_date' => 'Registration Date',
+    'expire_date' => 'Expire Date',
+    'status' => 'Status',
+    'report' => 'Report',
+];
