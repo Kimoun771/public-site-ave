dd9250a3234593872c4a0b0c266dc5278cc87f5d
sovouthea33 <sovouthea90@gmail.com>
2025-05-27 16:04:19 +0700

    task: translate the contact page
    resolve conflict in middleware

---
+++ lang/en/contact.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'contact_us' => 'Contact Us',
+    'map' => 'Map',
+    'get_in_touch' => 'Get In Touch With Us Now!',
+    'phone_number' => 'Phone Number',
+    'email' => 'Email',
+    'location' => 'Location',
+    'working_hour' => 'Working Hours',
+];
