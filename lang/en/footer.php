f7fe54de8bde4aa957a0b779fa9267fe54c793a8
Kimoun771 <118980482+Kimoun771@users.noreply.github.com>
2025-06-03 10:32:14 +0700

    task: translate footer

---
+++ lang/en/footer.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'name' => 'Name',
+    'email' => 'Email',
+    'message' => 'Message',
+    'submit' => 'Submit',
+    'copyright' => 'Copyright © 2025 AVE. All rights reserved.',
+];
